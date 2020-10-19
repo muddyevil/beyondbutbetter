d7249058b469fda1ae375568fcd9400ea4285ff3
Meriadoc Nathaniel <61142977+muddyevil@users.noreply.github.com>
2020-10-19 16:20:17 +0100

    Create info.js
    Javascript file for info pages

---
+++ info.js
@@ -0,0 +1,10 @@
+function fillArtificer() {
+
+document.getElementById("className").innerHTML = "Artificer";
+
+document.getElementById("classInfo").innerHTML = "Masters of unlocking magic in everyday objects, artificers are supreme inventors. They see magic as a complex system waiting to be decoded and controlled. Artificers use tools to channel arcane power, crafting magical objects. To cast a spell, an artificer could use alchemist's supplies to create a potent elixir, calligrapher's supplies to inscribe a sigil of power on an ally's armor, or tinker's tools to craft a temporary charm. The magic of artificers is tied to their tools and their talents.";
+
+document.getElementById("info1Title").innerHTML = "Arcane Science";
+
+document.getElementById("info1").innerHTML = "In the world of Eberron, arcane magic has been harnessed as a form of science and deployed throughout society. Artificers reflect this development. Their knowledge of magical devices, and their ability to infuse mundane items with magic, allows Eberron's most miraculous projects to continue. During the Last War, artificers were marshaled on a massive scale. Many lives were saved because of the inventions of brave artificers, but countless lives were also lost because of the mass destruction unleashed by their creations."
+};
