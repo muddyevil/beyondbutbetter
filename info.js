646f37c0cfc413036690a4aeffb39009c91e2d99
Meriadoc Nathaniel <61142977+muddyevil@users.noreply.github.com>
2020-10-19 17:24:14 +0100

    Update info.js
    Complete overhaul in order to implement top line tabs

--- info.js
+++ info.js
@@ -1,3 +1,531 @@
+// top navigation bar
+
+// races tab
+
+function raceTab() {
+
+// aarakocra
+
+let aarakocraLink = document.createElement("a");
+    
+aarakocraLink.innerHTML = "Aarakocra";
+aarakocraLink.setAttribute = ("class", "raceSideNav");
+aarakocraLink.href = "javascript:fillAaracokra()";
+
+document.getElementById("sideNav").appendChild(aarakocraLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+
+// aasimar
+
+let aasimarLink = document.createElement("a");
+    
+aasimarLink.innerHTML = "Aasimar";
+aasimarLink.setAttribute = ("class", "raceSideNav");
+aasimarLink.href = "javascript:fillAasimar()";
+
+document.getElementById("sideNav").appendChild(aasimarLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+
+// Bugbear
+
+let bugbearLink = document.createElement("a");
+    
+bugbearLink.innerHTML = "bugbear";
+bugbearLink.setAttribute = ("class", "raceSideNav");
+bugbearLink.href = "javascript:fillbugbear()";
+
+document.getElementById("sideNav").appendChild(bugbearLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+ 
+
+// Centaur
+
+let centaurLink = document.createElement("a");
+    
+centaurLink.innerHTML = "centaur";
+centaurLink.setAttribute = ("class", "raceSideNav");
+centaurLink.href = "javascript:fillcentaur()";
+
+document.getElementById("sideNav").appendChild(centaurLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+ 
+
+// changeling
+
+let changelingLink = document.createElement("a");
+    
+changelingLink.innerHTML = "changeling";
+changelingLink.setAttribute = ("class", "raceSideNav");
+changelingLink.href = "javascript:fillchangeling()";
+
+document.getElementById("sideNav").appendChild(changelingLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+ 
+
+// dragonborn
+
+let dragonbornLink = document.createElement("a");
+    
+dragonbornLink.innerHTML = "dragonborn";
+dragonbornLink.setAttribute = ("class", "raceSideNav");
+dragonbornLink.href = "javascript:filldragonborn()";
+
+document.getElementById("sideNav").appendChild(dragonbornLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+ 
+
+// dwarf
+
+let dwarfLink = document.createElement("a");
+    
+dwarfLink.innerHTML = "dwarf";
+dwarfLink.setAttribute = ("class", "raceSideNav");
+dwarfLink.href = "javascript:filldwarf()";
+
+document.getElementById("sideNav").appendChild(dwarfLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+ 
+
+// elf
+
+let elfLink = document.createElement("a");
+    
+elfLink.innerHTML = "elf";
+elfLink.setAttribute = ("class", "raceSideNav");
+elfLink.href = "javascript:fillelf()";
+
+document.getElementById("sideNav").appendChild(elfLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+ 
+
+// firbolg
+
+let firbolgLink = document.createElement("a");
+    
+firbolgLink.innerHTML = "firbolg";
+firbolgLink.setAttribute = ("class", "raceSideNav");
+firbolgLink.href = "javascript:fillfirbolg()";
+
+document.getElementById("sideNav").appendChild(firbolgLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+ 
+
+// genasi
+
+let genasiLink = document.createElement("a");
+    
+genasiLink.innerHTML = "genasi";
+genasiLink.setAttribute = ("class", "raceSideNav");
+genasiLink.href = "javascript:fillgenasi()";
+
+document.getElementById("sideNav").appendChild(genasiLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+ 
+
+// gith
+
+let githLink = document.createElement("a");
+    
+githLink.innerHTML = "gith";
+githLink.setAttribute = ("class", "raceSideNav");
+githLink.href = "javascript:fillgith()";
+
+document.getElementById("sideNav").appendChild(githLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+ 
+
+// gnome
+
+let gnomeLink = document.createElement("a");
+    
+gnomeLink.innerHTML = "gnome";
+gnomeLink.setAttribute = ("class", "raceSideNav");
+gnomeLink.href = "javascript:fillgnome()";
+
+document.getElementById("sideNav").appendChild(gnomeLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+ 
+
+// goblin
+
+let goblinLink = document.createElement("a");
+    
+goblinLink.innerHTML = "goblin";
+goblinLink.setAttribute = ("class", "raceSideNav");
+goblinLink.href = "javascript:fillgoblin()";
+
+document.getElementById("sideNav").appendChild(goblinLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+ 
+
+// goliath
+
+let goliathLink = document.createElement("a");
+    
+goliathLink.innerHTML = "goliath";
+goliathLink.setAttribute = ("class", "raceSideNav");
+goliathLink.href = "javascript:fillgoliath()";
+
+document.getElementById("sideNav").appendChild(goliathLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+ 
+
+// halfelf
+
+let halfelfLink = document.createElement("a");
+    
+halfelfLink.innerHTML = "halfelf";
+halfelfLink.setAttribute = ("class", "raceSideNav");
+halfelfLink.href = "javascript:fillhalfelf()";
+
+document.getElementById("sideNav").appendChild(halfelfLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+ 
+
+// halforc
+
+let halforcLink = document.createElement("a");
+    
+halforcLink.innerHTML = "halforc";
+halforcLink.setAttribute = ("class", "raceSideNav");
+halforcLink.href = "javascript:fillhalforc()";
+
+document.getElementById("sideNav").appendChild(halforcLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+ 
+
+// halfling
+
+let halflingLink = document.createElement("a");
+    
+halflingLink.innerHTML = "halfling";
+halflingLink.setAttribute = ("class", "raceSideNav");
+halflingLink.href = "javascript:fillhalfling()";
+
+document.getElementById("sideNav").appendChild(halflingLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+ 
+
+// hobgoblin
+
+let hobgoblinLink = document.createElement("a");
+    
+hobgoblinLink.innerHTML = "hobgoblin";
+hobgoblinLink.setAttribute = ("class", "raceSideNav");
+hobgoblinLink.href = "javascript:fillhobgoblin()";
+
+document.getElementById("sideNav").appendChild(hobgoblinLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+ 
+
+// human
+
+let humanLink = document.createElement("a");
+    
+humanLink.innerHTML = "human";
+humanLink.setAttribute = ("class", "raceSideNav");
+humanLink.href = "javascript:fillhuman()";
+
+document.getElementById("sideNav").appendChild(humanLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+ 
+
+// kalashtar
+
+let kalashtarLink = document.createElement("a");
+    
+kalashtarLink.innerHTML = "kalashtar";
+kalashtarLink.setAttribute = ("class", "raceSideNav");
+kalashtarLink.href = "javascript:fillkalashtar()";
+
+document.getElementById("sideNav").appendChild(kalashtarLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+ 
+
+// kenku
+
+let kenkuLink = document.createElement("a");
+    
+kenkuLink.innerHTML = "kenku";
+kenkuLink.setAttribute = ("class", "raceSideNav");
+kenkuLink.href = "javascript:fillkenku()";
+
+document.getElementById("sideNav").appendChild(kenkuLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+ 
+
+// kobold
+
+let koboldLink = document.createElement("a");
+    
+koboldLink.innerHTML = "kobold";
+koboldLink.setAttribute = ("class", "raceSideNav");
+koboldLink.href = "javascript:fillkobold()";
+
+document.getElementById("sideNav").appendChild(koboldLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+ 
+
+// leonin
+
+let leoninLink = document.createElement("a");
+    
+leoninLink.innerHTML = "leonin";
+leoninLink.setAttribute = ("class", "raceSideNav");
+leoninLink.href = "javascript:fillleonin()";
+
+document.getElementById("sideNav").appendChild(leoninLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+ 
+
+// Lizardfolk
+
+let lizardfolkLink = document.createElement("a");
+    
+lizardfolkLink.innerHTML = "lizardfolk";
+lizardfolkLink.setAttribute = ("class", "raceSideNav");
+lizardfolkLink.href = "javascript:filllizardfolk()";
+
+document.getElementById("sideNav").appendChild(lizardfolkLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+ 
+
+// loxodon
+
+let loxodonLink = document.createElement("a");
+    
+loxodonLink.innerHTML = "loxodon";
+loxodonLink.setAttribute = ("class", "raceSideNav");
+loxodonLink.href = "javascript:fillloxodon()";
+
+document.getElementById("sideNav").appendChild(loxodonLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+ 
+
+// minotaur
+
+let minotaurLink = document.createElement("a");
+    
+minotaurLink.innerHTML = "minotaur";
+minotaurLink.setAttribute = ("class", "raceSideNav");
+minotaurLink.href = "javascript:fillminotaur()";
+
+document.getElementById("sideNav").appendChild(minotaurLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+ 
+
+// orc
+
+let orcLink = document.createElement("a");
+    
+orcLink.innerHTML = "orc";
+orcLink.setAttribute = ("class", "raceSideNav");
+orcLink.href = "javascript:fillorc()";
+
+document.getElementById("sideNav").appendChild(orcLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+ 
+
+// Satyr
+
+let satyrLink = document.createElement("a");
+    
+satyrLink.innerHTML = "satyr";
+satyrLink.setAttribute = ("class", "raceSideNav");
+satyrLink.href = "javascript:fillsatyr()";
+
+document.getElementById("sideNav").appendChild(satyrLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+ 
+
+// shifter
+
+let shifterLink = document.createElement("a");
+    
+shifterLink.innerHTML = "shifter";
+shifterLink.setAttribute = ("class", "raceSideNav");
+shifterLink.href = "javascript:fillshifter()";
+
+document.getElementById("sideNav").appendChild(shifterLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+ 
+
+// Simic Hybrid
+
+let simicLink = document.createElement("a");
+    
+simicLink.innerHTML = "simic";
+simicLink.setAttribute = ("class", "raceSideNav");
+simicLink.href = "javascript:fillsimic()";
+
+document.getElementById("sideNav").appendChild(simicLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+ 
+
+// tabaxi
+
+let tabaxiLink = document.createElement("a");
+    
+tabaxiLink.innerHTML = "tabaxi";
+tabaxiLink.setAttribute = ("class", "raceSideNav");
+tabaxiLink.href = "javascript:filltabaxi()";
+
+document.getElementById("sideNav").appendChild(tabaxiLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+
+// tiefling 
+
+let tieflingLink = document.createElement("a");
+    
+tieflingLink.innerHTML = "tiefling";
+tieflingLink.setAttribute = ("class", "raceSideNav");
+tieflingLink.href = "javascript:filltiefling()";
+
+document.getElementById("sideNav").appendChild(tieflingLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+
+// Triton
+
+let tritonLink = document.createElement("a");
+    
+tritonLink.innerHTML = "triton";
+tritonLink.setAttribute = ("class", "raceSideNav");
+tritonLink.href = "javascript:filltriton()";
+
+document.getElementById("sideNav").appendChild(tritonLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+
+// Vedalken
+
+let vedalkanLink = document.createElement("a");
+    
+vedalkanLink.innerHTML = "vedalkan";
+vedalkanLink.setAttribute = ("class", "raceSideNav");
+vedalkanLink.href = "javascript:fillvedalkan()";
+
+document.getElementById("sideNav").appendChild(vedalkanLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+
+// Verdan
+
+let verdanLink = document.createElement("a");
+    
+verdanLink.innerHTML = "verdan";
+verdanLink.setAttribute = ("class", "raceSideNav");
+verdanLink.href = "javascript:fillverdan()";
+
+document.getElementById("sideNav").appendChild(verdanLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+
+// Warforged
+
+let warforgedLink = document.createElement("a");
+    
+warforgedLink.innerHTML = "warforged";
+warforgedLink.setAttribute = ("class", "raceSideNav");
+warforgedLink.href = "javascript:fillwarforged()";
+
+document.getElementById("sideNav").appendChild(warforgedLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+
+// Tuan-ti pureblood
+
+let yuanLink = document.createElement("a");
+    
+yuanLink.innerHTML = "yuan";
+yuanLink.setAttribute = ("class", "raceSideNav");
+yuanLink.href = "javascript:fillyuan()";
+
+document.getElementById("sideNav").appendChild(yuanLink);
+
+var br = document.createElement("br");
+document.getElementById("sideNav").appendChild(br);
+
+};
+
+
+
+// classes tab
+
+// background tab
+
+// feats tab
+
+
+// classes tab side bar
+
 function fillArtificer() {
 
 document.getElementById("className").innerHTML = "Artificer";
